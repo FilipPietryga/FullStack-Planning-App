86e59d5f2c542a518ce2de4947226fa662151218
FilipPietryga <filippatrykpietryga@gmail.com>
2024-01-24 00:31:21 +0100

    introduced repository

--- backend/src/main/java/backend/planning/repositories/TaskRepository.java
+++ backend/src/main/java/backend/planning/repositories/TaskRepository.java
@@ -1,4 +1,9 @@
 package backend.planning.repositories;
 
-public class TaskRepository {
-}
+import backend.planning.entities.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends MongoRepository<Task,String> {}
+
